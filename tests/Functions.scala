1e1cd3d58b72270c20be3ec01e072990bc5de204
Mae.Klinkenberg <mae.klinkenberg@student.uliege.be>
2022-11-10 23:05:55 +0100

    des tests

--- tests/Functions.scala
+++ tests/Functions.scala
@@ -2,6 +2,7 @@ package tests
 
 import project.Operators.LogicalOp, LogicalOp.*
 import project.Functions.semtab, project.Tableau
+import project.Functions.isSatisfiable, project.Functions.isContradiction, project.Functions.isTautology
 
 object Semtab extends TestFramework:
     def testProjectStatement: Result[Tableau] =
@@ -36,3 +37,53 @@ object Semtab extends TestFramework:
 
         this.assertEqual(semtab(formula2), output2)
 
+
+object IsSatisfiable extends TestFramework:
+	def test: Result[Boolean] =
+		val formula: List[LogicalOp] = List(
+          Or(Atom("p"), Atom("q")),
+          Atom("r"),
+          Not(Atom("q")),
+        )
+
+		val output = true
+
+		this.assertEqual(isSatisfiable(formula), output)
+
+object IsTautology extends TestFramework:
+	def test: Result[Boolean] =
+		val formula: List[LogicalOp] = List(
+          Or(Atom("p"), Atom("q")),
+          Atom("r"),
+          Not(Atom("q")),
+        )
+
+		val f: List[LogicalOp] = List(
+			IfThen(And(IfThen(Atom("p"), Atom("q")),
+			Atom("p")), Atom("q"))
+		)
+
+		val output = false
+		val o = true
+
+		this.assertEqual(isTautology(formula), output)
+		this.assertEqual(isTautology(f), o)
+
+object IsContradiction extends TestFramework:
+	def test: Result[Boolean] =
+		val formula: List[LogicalOp] = List(
+          Or(Atom("p"), Atom("q")),
+          Atom("r"),
+          Not(Atom("q")),
+        )
+
+		val f: List[LogicalOp] = List(
+			And(Atom("p"), Not(Atom("p")))
+		)
+
+		val output = false
+		val o = true
+
+		this.assertEqual(isContradiction(formula), output)
+
+		this.assertEqual(isContradiction(f), o)
